fc48f9c91c58fb792ec70a035e977418f114b723
Yan Maia <134809137+IIYanMaiaII@users.noreply.github.com>
2025-07-01 14:32:47 -0300

    Add files via upload
    reenviando aqui, acho que tinha feito o commit errado

--- in/c08.pas
+++ in/c08.pas
@@ -1,15 +1,26 @@
-{ Sample program in Pascal - basic mix of types in write }
-program c08;
+{ Sample program in Pascal -
+  silly program to handle some corner cases.
+}
+
+program c09;
+var
+    xxxx: integer;
+    yyy: integer;
+    longidentifier: integer;
+    mensagem: string;  { Declaração da string }
+
 begin
-    write('4 + 2 = ');
-    writeln(4 + 2);
+    mensagem := 'Esta é uma string declarada previamente.';
+    
+    writeln('Silly program.');
+    writeln(4 + 3 * 2);             { Should write 10 not 14. }
+    writeln('Silly program.');     { Should produce one entry in table. }
+    
+    xxxx := 4;
+    longidentifier := 4;
+    yyy := longidentifier;
+    
+    writeln((xxxx * yyy) div longidentifier); { Should write 4. }
 
-    write('5 - 2 = ');
-    writeln(5 - 2);
-
-    write('4 * 2 = ');
-    writeln(4 * 2);
-
-    write('4 / 2 = ');
-    writeln(4 / 2);
+    writeln(mensagem);  { Impressão da string declarada }
 end.
